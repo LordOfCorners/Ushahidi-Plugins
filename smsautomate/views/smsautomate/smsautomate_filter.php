66ec9997bcb6f028bafbcca8df2a4c2966fefe50
Leif Percifield <lpercifield@gmail.com>
2013-06-04 15:44:00 -0600

    testing filter plugin

---
+++ smsautomate/views/smsautomate/smsautomate_filter.php
@@ -0,0 +1,24 @@
+<!--
+				<?php
+				// Action::main_sidebar_post_filters - Add Items to the Entry Page after filters
+				Event::run('ushahidi_action.main_sidebar_post_filters');
+				?>
+-->
+<div id="report-type-filter" class="filters">
+					<h3><?php echo Kohana::lang('ui_main.type'); ?></h3>
+						<ul>
+							<li><a id="media_0" class="active" href="#"><span><?php echo Kohana::lang('ui_main.reports'); ?></span></a></li>
+							<li><a id="media_4" href="#"><span><?php echo Kohana::lang('ui_main.news'); ?></span></a></li>
+							<li><a id="media_1" href="#"><span><?php echo Kohana::lang('ui_main.pictures'); ?></span></a></li>
+							<li><a id="media_2" href="#"><span><?php echo Kohana::lang('ui_main.video'); ?></span></a></li>
+							<li><a id="media_0" href="#"><span><?php echo Kohana::lang('ui_main.all'); ?></span></a></li>
+						</ul>
+						<div class="floatbox">
+
+							</div>
+							<!-- / report type filters -->
+											<?php
+				// Action::main_sidebar_post_filters - Add Items to the Entry Page after filters
+				Event::run('ushahidi_action.main_sidebar_post_filters');
+				?>
+				</div>
