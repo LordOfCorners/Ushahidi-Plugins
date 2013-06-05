2b92f0a711a1674e62f24ecbd78c2fef2b5cbba5
Michael <kaham340@newschool.edu>
2013-06-05 09:27:22 -0600

    Translation test

---
+++ smsautomate/i18n/es_ES/smsautomate_ui.php
@@ -0,0 +1,16 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Spanish language file for SMSAutomate plugin
+ *
+ * PHP version 5
+ * LICENSE: This source file is subject to LGPL license 
+ * that is available through the world-wide-web at the following URI:
+ * http://www.gnu.org/copyleft/lesser.html
+ * @author	   Michael Kahane <kaham340@newschool.edu> 
+ * @package    Ushahidi - http://source.ushahididev.com
+ * @license    http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (LGPL) 
+ */
+	$lang = array(
+	'bad_location' => 'Location code not found (in spanish)',
+	);
+?>
