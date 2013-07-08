f790cd113c6b95a9a65b638b5c78e361e04da530
Michael <kaham340@newschool.edu>
2013-07-08 15:02:35 -0600

    Actionable Translations

---
+++ actionable/i18n/es_AR/actionable.php
@@ -0,0 +1,13 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+	$lang = array(
+		'actionable' => 'Procesable',
+		'not_actionable' => 'No Procesable',
+		'urgent' => 'Urgente',
+		'action_taken' => 'Problema Resuelto',
+		'action_status_description' => 'Este reporte necesita una solución?',
+		'action_summary_description' => 'Se han tomado las medidas? Resumir la acción tomada o requerida en el presente informe',
+		'summary' => 'Resumen',
+		'action_needed' => 'Acción necesaria',
+		'action_urgent' => 'Acción necesaria urgente'
+	);
+?>
